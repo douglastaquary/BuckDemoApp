4d41526ac657cd77a895a182bb8ab619f9fab9e7
Douglas Taquary (n225862) <douglas.taquary@santander.com.br>
2020-01-23 10:53:35 -0300

    Initial Buck setup.

---
+++ BuckDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuckDemoApp
+//
+//  Created by Douglas Alexandre Barros Taquary on 23/01/20.
+//  Copyright © 2020 Douglas Taquary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
